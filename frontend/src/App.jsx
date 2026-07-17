61ae88cb0ed804d3a7b2f5caf50435bd07a11374
Mystifying7 <rehanalam201006@gmail.com>
2026-07-18 01:23:04 +0530

    Add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,8 @@ export default function App() {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch('https://bangalore-house-price-api.onrender.com/api/metadata', {
+      const response = await fetch('https://bangalore-house-price-api.onrender.com/api/metadata'
+      , {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
