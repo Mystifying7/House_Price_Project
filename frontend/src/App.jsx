a501e5d57861a9290dd8dfc4361407dbc8b492c6
Mystifying7 <rehanalam201006@gmail.com>
2026-07-18 01:22:39 +0530

    Added backend link in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
 
   // Fetch unique locations from Flask backend on mount
   useEffect(() => {
-    fetch('http://localhost:5000/api/metadata')
+    fetch('https://bangalore-house-price-api.onrender.com/api/metadata')
       .then(res => res.json())
       .then(data => {
         setLocations(data.locations);
@@ -23,7 +23,7 @@ export default function App() {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/predict', {
+      const response = await fetch('https://bangalore-house-price-api.onrender.com/api/metadata', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
